c19027be5d4c64158960cd5e7d866d0e2ab42941
manoluky <46540583+manoluky@users.noreply.github.com>
2024-03-21 12:57:50 -0300

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -6,10 +6,8 @@ NAV=$4
 cd /opt
 chmod +x clone.sh
 chmod +x testgradle.sh
-chmod +x report.sh
 sh clone.sh ${RAMA} ${REPOSITORIO}
 cd /opt/framework
 cp /opt/testgradle.sh /opt/framework
-cp /opt/report.sh /opt/framework
 sh testgradle.sh ${TAG} ${NAV}
 
