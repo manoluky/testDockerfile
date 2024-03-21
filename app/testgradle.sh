05b9174870fc0807288da1c2beea1b6673f33739
manoluky <46540583+manoluky@users.noreply.github.com>
2024-03-21 15:59:15 -0300

    Update testgradle.sh

--- app/testgradle.sh
+++ app/testgradle.sh
@@ -3,5 +3,5 @@ TAG=$1
 NAV=$2
 
 gradle build --refresh-dependencies
-#xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV || true
-#ls /opt/framework/results
+xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV || true
+
